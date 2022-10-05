fe39f87310c829418dc8ffcb0209941b8a6f3391
Rocio <roci0055@easv365.dk>
2022-10-05 21:20:18 +0200

    upload

---
+++ CompulsoryAssignment/PrimeGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace CompulsoryAssignment
+{
+    public class PrimeGenerator
+    {
+        //1) First implement the function using a sequential approach, and check for correctness
+
+        public List<long> GetPrimesSequential(long first, long last)
+        {
+            List<long> primeNumbers = new List<long>();
+
+            for(long i = first; i<=last; i++)
+                if(isPrimeNumber(i)) primeNumbers.Add(i);
+            
+            return primeNumbers;
+        }
+
+        //2) Then implement the function using parallel programming, and check for correctness (take care of race conditions
+        //when writing in parallel to the list).
+
+        public List<long> GetPrimesParallel(long first, long last)
+        {
+            List<long> primeNumbers = new List<long>();
+            Object lockObject = new();
+
+            Parallel.ForEach(Partitioner.Create(first, last), range =>
+            {
+                List<long> localList = new List<long>();
+                for (long i = range.Item1; i < range.Item2; i++)
+                {
+                    if(isPrimeNumber(i)) localList.Add(i);
+                }
+                lock (lockObject) 
+                {
+                    primeNumbers.AddRange(localList);
+                }
+
+            });
+
+            primeNumbers.Sort();
+            return primeNumbers;
+        }
+
+        public bool isPrimeNumber(long number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        public void performanceMeasurer(Action action)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            action.Invoke();
+            stopwatch.Stop();
+            Console.Write(stopwatch.Elapsed.TotalMilliseconds+"\n");
+        }
+    }
+}
---
+++ CompulsoryAssignment/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CompulsoryAssignment;
+using System.Management;
+
+PrimeGenerator generator = new PrimeGenerator();
+
+ManagementObjectSearcher mos =
+  new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Processor");
+foreach (ManagementObject mo in mos.Get())
+{
+    Console.Write("RUNNING ON: ");
+    Console.Write(mo["Name"]);
+    Console.WriteLine("");
+}
+
+//3) Compare the performance of both methods for various ranges, like:
+
+//a.
+Console.WriteLine("\na. 1 – 1.000.000");
+Console.Write("\n Total time in sequential:\t");
+generator.performanceMeasurer(() => generator.GetPrimesSequential(1, 1000000));
+Console.Write("\n Total time parallel:\t\t");
+generator.performanceMeasurer(() => generator.GetPrimesParallel(1, 1000000));
+
+//b.
+Console.WriteLine("\nb. 1 – 10.000.000");
+Console.Write("\n Total time in sequential:\t");
+generator.performanceMeasurer(() => generator.GetPrimesSequential(1, 10000000));
+Console.Write("\n Total time parallel:\t\t");
+generator.performanceMeasurer(() => generator.GetPrimesParallel(1, 10000000));
+//c.
+Console.WriteLine("\nc. 1.000.000 – 2.000.000");
+Console.Write("\n Total time in sequential:\t");
+generator.performanceMeasurer(() => generator.GetPrimesSequential(1000000, 2000000));
+Console.Write("\n Total time parallel:\t\t");
+generator.performanceMeasurer(() => generator.GetPrimesParallel(1000000, 2000000));
+//d.
+Console.WriteLine("\nd. 10.000.000 – 20.000.000");
+Console.Write("\n Total time in sequential:\t");
+generator.performanceMeasurer(() => generator.GetPrimesSequential(10000000, 20000000));
+Console.Write("\n Total time parallel:\t\t");
+generator.performanceMeasurer(() => generator.GetPrimesParallel(10000000, 20000000));
+
+Console.ReadKey();
+
+
+
